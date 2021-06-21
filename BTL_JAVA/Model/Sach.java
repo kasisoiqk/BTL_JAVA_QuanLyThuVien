32e8cd49d4a76b6e13c74ed849826a5cc7f1091d
Nguyen Quoc Khanh <74106038+kasisoiqk@users.noreply.github.com>
2021-06-21 20:37:18 +0700

    Add files via upload

---
+++ BTL_JAVA/Model/Sach.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Laptopkhanhtran.vn
+ */
+public class Sach {
+    private  int maSach;
+    private String tenSach;
+    private String tacGia;
+    private String nhaCungCap;
+    private String ngayNhap;
+    private int soLuongTong;
+    private int soLuongCon;
+    private String theLoai;
+    private int giaSach;
+
+    public Sach() {
+    }
+
+    public Sach(int maSach, String tenSach, String tacGia, String nhaCungCap, String ngayNhap, int soLuongTong, int soLuongCon, String theLoai, int giaSach) {
+        this.maSach = maSach;
+        this.tenSach = tenSach;
+        this.tacGia = tacGia;
+        this.nhaCungCap = nhaCungCap;
+        this.ngayNhap = ngayNhap;
+        this.soLuongTong = soLuongTong;
+        this.soLuongCon = soLuongCon;
+        this.theLoai = theLoai;
+        this.giaSach = giaSach;
+    }
+
+    public int getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(int maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public String getNhaCungCap() {
+        return nhaCungCap;
+    }
+
+    public void setNhaCungCap(String nhaCungCap) {
+        this.nhaCungCap = nhaCungCap;
+    }
+
+    public String getNgayNhap() {
+        return ngayNhap;
+    }
+
+    public void setNgayNhap(String ngayNhap) {
+        this.ngayNhap = ngayNhap;
+    }
+
+    public int getSoLuongTong() {
+        return soLuongTong;
+    }
+
+    public void setSoLuongTong(int soLuongTong) {
+        this.soLuongTong = soLuongTong;
+    }
+
+    public int getSoLuongCon() {
+        return soLuongCon;
+    }
+
+    public void setSoLuongCon(int soLuongCon) {
+        this.soLuongCon = soLuongCon;
+    }
+
+    public String getTheLoai() {
+        return theLoai;
+    }
+
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
+
+    public int getGiaSach() {
+        return giaSach;
+    }
+
+    public void setGiaSach(int giaSach) {
+        this.giaSach = giaSach;
+    }
+    public void nhap(){
+        System.out.print("Nhập mã sách: ");
+        maSach=new Scanner(System.in).nextInt();
+        System.out.print("Nhập tên sách: ");
+        tenSach=new Scanner(System.in).nextLine();
+        System.out.print("Nhập tên tác giả: ");
+        tacGia=new Scanner(System.in).nextLine();
+        System.out.print("Nhập tên nhà cung cấp: ");
+        nhaCungCap=new Scanner(System.in).nextLine();
+        System.out.print("Nhập thể loại: ");
+        theLoai=new Scanner(System.in).nextLine();
+        System.out.print("Nhập ngày nhập sách: ");
+        ngayNhap=new Scanner(System.in).nextLine();
+        System.out.print("Nhập số lượng tổng: ");
+        soLuongTong=new Scanner(System.in).nextInt();
+        System.out.print("Nhập số lượng còn lại: ");
+        soLuongCon=new Scanner(System.in).nextInt();
+        System.out.print("Nhập giá sách: ");
+        giaSach=new Scanner(System.in).nextInt();
+        
+    }
+    public void xuat(){
+         System.out.format("| %20s | %20s | %20s | %20s | %20s | %20s | %20s | %20s | %20s \n ", maSach, tenSach, tacGia, nhaCungCap, theLoai, ngayNhap, soLuongTong, soLuongCon,giaSach);
+    }
+    
+}
